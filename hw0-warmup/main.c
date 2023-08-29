2877c11ee73dbce74ac01a8b34cfd620b719b24d
Eric Weng <qgt7zm@virginia.edu>
2023-08-29 14:57:03 -0400

    Get sep from argv

--- hw0-warmup/main.c
+++ hw0-warmup/main.c
@@ -1,14 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "split.h"
 
+// TODO this should be on stack
+char *getSep(int argc, char *argv[]) {
+	const int minIdx = 1;
+
+	if (argc == minIdx) {
+		char *sep = calloc(sizeof(char), 2 + 1);
+		strcpy(sep, " \t");
+		return sep;
+	}
+
+	// Create string
+	int sepLen = 0;
+	int curIdx = sepLen;
+	char *sep = (char *) calloc(sizeof(char), sepLen + 1);
+
+	for (int i = minIdx; i < argc; i++) {
+		// Grow string
+		sepLen += strlen(argv[i]);
+		sep = (char *) realloc(sep, sepLen + 1);
+		// Concatenate next arg
+		strcpy(sep + curIdx, argv[i]);
+		curIdx = sepLen;
+	}
+	return sep;
+}
+
 int main(int argc, char *argv[]) {
     const char *input = "Computer Systems and Organization 2";
-    const char *sep = "CSO2";
+    char *sep = getSep(argc, argv);
     int size;
     char **result = string_split(input, sep, &size);
-    printf("%s %s %d\n", input, sep, size);
-    printf("%s\n", result[0]);
-	printf("Done\n");
+
+    printf("Input: %s\n", input);
+	printf("Sep: %s\n", sep);
+	printf("Size: %d\n", size);
+    printf("Result: %s\n", result[0]);
+
+	free(sep);
     return 0;
 }
