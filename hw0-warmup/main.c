b8b060a5fb3da896ae72c472450e6eeb2306d323
Eric Weng <qgt7zm@virginia.edu>
2023-08-29 14:57:32 -0400

    Add tests for sep

--- hw0-warmup/main.c
+++ hw0-warmup/main.c
@@ -30,17 +30,42 @@ char *getSep(int argc, char *argv[]) {
 	return sep;
 }
 
+void test_sep(char *expected, int argc, char *argv[]) {
+	char *result = getSep(argc, argv);
+	if (strcmp(expected, result) == 0) {
+		printf("Test passed\n");
+	} else {
+		printf("Test failed; expected '%s'\n", expected);
+	}
+	free(result);
+}
+
+void test() {
+	char* argv0[] = {"split"};
+	test_sep(" \t", 1, argv0);
+
+	char* argv1[] = {"split", "abc"};
+	test_sep("abc", 2, argv1);
+
+	char* argv2[] = {"split", "ab", "c"};
+	test_sep("abc", 3, argv2);
+
+	char* argv3[] = {"split", "a", "b", "c"};
+	test_sep("abc", 4, argv3);
+}	
+
 int main(int argc, char *argv[]) {
     const char *input = "Computer Systems and Organization 2";
-    char *sep = getSep(argc, argv);
-    int size;
-    char **result = string_split(input, sep, &size);
+	test();
 
-    printf("Input: %s\n", input);
-	printf("Sep: %s\n", sep);
-	printf("Size: %d\n", size);
-    printf("Result: %s\n", result[0]);
+    // char *sep = getSep(argc, argv);
+    // int size;
+    // char **result = string_split(input, sep, &size);
+    // printf("Input: %s\n", input);
+	// printf("Sep: %s\n", sep);
+	// printf("Size: %d\n", size);
+    // printf("Result: %s\n", result[0]);
 
-	free(sep);
+	// free(sep);
     return 0;
 }
