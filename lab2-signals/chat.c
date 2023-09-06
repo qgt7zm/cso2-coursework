88a832c3932eb6a6c325df29815161ebf4a61d9e
Eric Weng <qgt7zm@virginia.edu>
2023-09-06 19:02:11 -0400

    Setup signal actions and handler

--- lab2-signals/chat.c
+++ lab2-signals/chat.c
@@ -1,5 +1,55 @@
+# include <signal.h>
 # include <stdio.h>
+# include <unistd.h>
+
+void printPid() {
+	pid_t processPid = getpid();
+	printf("Process ID: %d\n", processPid);
+}
+
+void displayInbox() {
+	printf("Display inbox\n");
+}
+
+void stopOtherProgram() {
+	printf("Stop other program\n");
+}
+
+void cleanup() {
+	printf("Cleanup\n");
+}
+
+static void handleSignal(int signum) {
+    if (signum == SIGTERM) {
+		// Kill: Cleanup (clear inbox/outbox)
+		cleanup();
+	} else if (signum == SIGINT) {
+		// Ctrl-C: Cleanup and kill other program
+		cleanup();
+		stopOtherProgram();
+	} else if (signum == SIGUSR1) {
+		// User-defined: display inbox and reset
+		displayInbox();
+	}
+	// Don't care about others
+}
 
 int main() {
 	printf("Hello there!\n");
+	printPid();
+
+	// Create signal handler
+	struct sigaction action;
+	action.sa_handler = handleSignal;
+	sigemptyset(&action.sa_mask);
+	action.sa_flags = SA_RESTART;
+
+	// Register signal actions
+	sigaction(SIGINT, &action, NULL);
+	sigaction(SIGTERM, &action, NULL);
+	sigaction(SIGUSR1, &action, NULL);
+
+	kill(getpid(), SIGUSR1);
+	kill(getpid(), SIGINT);
+	kill(getpid(), SIGTERM);
 }
