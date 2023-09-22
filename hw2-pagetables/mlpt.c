0a1553a917e17d5715e10cc104f829f263cf9e3f
Eric Weng <qgt7zm@virginia.edu>
2023-09-22 17:48:04 -0400

    Make library default output

---
+++ hw2-pagetables/mlpt.c
@@ -0,0 +1,15 @@
+#include "config.h"
+#include "mlpt.h"
+
+#include "util.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t translate(size_t va){
+    return 0;
+}
+
+void page_allocate(size_t va) {
+    return;
+}
