9a3038c69d4299f3335202e31b9fc55373684103
Eric Weng <qgt7zm@virginia.edu>
2023-10-23 22:35:57 -0400

    Add HW 5 header

---
+++ hw5-tlb/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/** The number of PTEs used to translate one address. */
+#define LEVELS 1
+
+/** The number of bits used for the page offset. */
+#define POBITS 12
+
+#endif
