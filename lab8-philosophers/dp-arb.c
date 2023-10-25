8fd3c5b8214d0e1cf75d2f3de9597165d5aaf75c
Eric Weng <qgt7zm@virginia.edu>
2023-10-25 13:33:27 -0400

    Implement arbitrator approach

--- lab8-philosophers/dp-arb.c
+++ lab8-philosophers/dp-arb.c
@@ -5,10 +5,15 @@
 
 pthread_t philosopher[5];
 pthread_mutex_t chopstick[5];
+pthread_mutex_t arbitrator;
 
-/** Dining philosophers problem with deadlock */
+/** Dining philosophers problem with arbitrator approach */
 void *eat(void *arg) {
     int n = (int) (long)arg;
+
+    // call the waiter
+    pthread_mutex_lock(&arbitrator);
+
     // take two chopsticks
     pthread_mutex_lock(&chopstick[n]);
     printf("Philosopher %d got chopstick %d\n", n, n);
@@ -23,12 +28,16 @@ void *eat(void *arg) {
     pthread_mutex_unlock(&chopstick[(n+1)%5]);
     printf("Philosopher %d set down chopstick %d\n", n, n);
     pthread_mutex_unlock(&chopstick[n]);
+
+    // dismiss the waiter
+    pthread_mutex_unlock(&arbitrator);
     return NULL;
 }
 
 int main(int argc, const char *argv[]) {
     for(int i = 0; i < 5; i += 1)
         pthread_mutex_init(&chopstick[i], NULL);
+    pthread_mutex_init(&arbitrator, NULL);
 
     for(int i =0; i < 5; i += 1)
         pthread_create(&philosopher[i], NULL, eat, (void *)(size_t)i);
@@ -38,6 +47,7 @@ int main(int argc, const char *argv[]) {
 
     for(int i=0; i < 5; i += 1)
         pthread_mutex_destroy(&chopstick[i]);
+    pthread_mutex_destroy(&arbitrator);
 
     return 0;
 }
