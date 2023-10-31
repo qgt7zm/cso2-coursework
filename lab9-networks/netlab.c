1f02563a3961330202928653b9b7d5770400dfb8
Eric Weng <qgt7zm@virginia.edu>
2023-10-31 16:04:24 -0400

    Complete netlab 1

--- lab9-networks/netlab.c
+++ lab9-networks/netlab.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 #include "netsim.h"
 
+int msg_count = 0;
+
+void foo(void* _data) {
+    printf("Your mom\n");
+}
+
 void recvd(size_t len, void* _data) {
     // FIX ME -- add proper handling of messages
     char *data = _data;
+    // printf("Received message %d = %s\n", msg_count, data);
     fwrite(data+3,1,len-3,stdout);
     fflush(stdout);
+
+    msg_count += 1;
+
+    char msg[6];
+    msg[1] = 'A';
+    msg[2] = 'C';
+    msg[3] = 'K';
+    msg[4] = msg_count;
+    // printf("Reply = %s\n", msg);
+    
+    char checksum = msg[1] ^ msg[2] ^ msg[3] ^ msg[4];
+    msg[0] = checksum;
+    send(5, msg);
+    // setTimeout(*foo, 1000, NULL);
 }
 
 int main(int argc, char *argv[]) {
@@ -18,10 +39,12 @@ int main(int argc, char *argv[]) {
     data[1] = 'G'; data[2] = 'E'; data[3] = 'T'; data[4] = argv[1][0];
     // end of working code
     
-    
-    data[0] = 102; // FIX ME -- add proper checksum computation
+    char checksum = data[1] ^ data[2] ^ data[3] ^ data[4];
+    data[0] = checksum; 
+    // printf("Data = %s\n", data);
     send(5, data);
     // FIX ME -- add action if no reply
+    // setTimeout(*foo, 1000, NULL);
 
     waitForAllTimeouts();
 }
