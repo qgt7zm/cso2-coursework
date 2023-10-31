770f1d764e5749e1248d6836b671493d89e849fa
Eric Weng <qgt7zm@virginia.edu>
2023-10-31 16:13:35 -0400

    Add helper functions for send

--- lab9-networks/netlab.c
+++ lab9-networks/netlab.c
@@ -1,32 +1,47 @@
 #include <stdio.h>
 #include "netsim.h"
 
-int msg_count = 0;
+char level; // Command-line argument
+int msg_count = 0; // Number of messages received
 
 void foo(void* _data) {
     printf("Your mom\n");
 }
 
+void send_get() {
+    char data[5];
+    data[1] = 'G';
+    data[2] = 'E';
+    data[3] = 'T';
+    data[4] = level;
+    
+    char checksum = data[1] ^ data[2] ^ data[3] ^ data[4];
+    data[0] = checksum; 
+    // printf("Data = %s\n", data);
+    send(5, data);
+}
+
+void send_ack() {
+    char msg[6];
+    msg[1] = 'A';
+    msg[2] = 'C';
+    msg[3] = 'K';
+    msg[4] = (char) msg_count;
+    // printf("Reply = %s\n", msg);
+
+    char checksum = msg[1] ^ msg[2] ^ msg[3] ^ msg[4];
+    msg[0] = checksum;
+    send(5, msg);
+}
+
 void recvd(size_t len, void* _data) {
-    // FIX ME -- add proper handling of messages
     char *data = _data;
     // printf("Received message %d = %s\n", msg_count, data);
     fwrite(data+3,1,len-3,stdout);
     fflush(stdout);
 
     msg_count += 1;
-
-    char msg[6];
-    msg[1] = 'A';
-    msg[2] = 'C';
-    msg[3] = 'K';
-    msg[4] = msg_count;
-    // printf("Reply = %s\n", msg);
-    
-    char checksum = msg[1] ^ msg[2] ^ msg[3] ^ msg[4];
-    msg[0] = checksum;
-    send(5, msg);
-    // setTimeout(*foo, 1000, NULL);
+    send_ack();
 }
 
 int main(int argc, char *argv[]) {
@@ -35,14 +50,10 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "USAGE: %s n\n    where n is a number between 0 and 3\n", argv[0]);
         return 1;
     }
-    char data[5];
-    data[1] = 'G'; data[2] = 'E'; data[3] = 'T'; data[4] = argv[1][0];
+    level = argv[1][0];
     // end of working code
     
-    char checksum = data[1] ^ data[2] ^ data[3] ^ data[4];
-    data[0] = checksum; 
-    // printf("Data = %s\n", data);
-    send(5, data);
+    send_get();
     // FIX ME -- add action if no reply
     // setTimeout(*foo, 1000, NULL);
 
