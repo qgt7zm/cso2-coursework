2e51cf9bfae7e13bfa7dc235379e0bc23c971c5f
Eric Weng <qgt7zm@virginia.edu>
2023-11-08 14:56:42 -0500

    Swap states when all threads finished

--- hw6-life/life-parallel.c
+++ hw6-life/life-parallel.c
@@ -4,13 +4,25 @@
 struct life_info {
     pthread_barrier_t barrier;
     int steps;
+    int threads;
+    int threads_completed;
+    LifeBoard *state;
+    LifeBoard *next_state;
 };
 
-void *life_threaded(void *args) {
-    struct life_info *info = (struct life_info *) args;
+void *life_threaded(void *arg) {
+    struct life_info *info = (struct life_info *) arg;
 
     for (int step = 0; step < info->steps; step++) {
+        printf("- Waiting step %d\n", step);
+        info->threads_completed += 1;
         pthread_barrier_wait(&info->barrier);
+
+        if (info->threads_completed == info->threads) {
+            LB_swap(info->state, info->next_state);
+            info->threads_completed = 0;
+            puts("Swapped states");
+        }   
     }
     return NULL;
 }
@@ -20,21 +32,28 @@ void simulate_life_parallel(int threads, LifeBoard *state, int steps) {
     struct life_info info;
     pthread_barrier_init(&info.barrier, NULL, threads);
 
-    LifeBoard *next_state = LB_new(state->width, state->height); // Save the state of the next step
-
     // Partition the board roughly evenly
     int slice_width = state->width / threads;
     if (slice_width == 0) slice_width = 1;
-    
+
+    // Step up the function argument
+    info.steps = steps;
+    LifeBoard *next_state = LB_new(state->width, state->height); // Save the state of the next step
+    info.next_state = next_state;
+    info.threads = threads;
+    info.threads_completed = 0;
+    info.state = state;
+    info.next_state = next_state;
+
     // Create all threads and run the game
     pthread_t game_threads[threads];
     for (int i = 0; i < threads; i++) {
-	pthread_create(&game_threads[i], NULL, life_threaded, (void *) &info);
+	    pthread_create(&game_threads[i], NULL, life_threaded, (void *) &info);
     }
 
     // Delete all threads
     for (int i = 0; i < threads; i++) {
-	pthread_join(game_threads[i], NULL);
+	    pthread_join(game_threads[i], NULL);
     }
 
     // Free all resources
