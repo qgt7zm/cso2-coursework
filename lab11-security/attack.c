38ad311f50a19ce52c75599661b1892b81c8027a
Eric Weng <qgt7zm@virginia.edu>
2023-11-14 17:29:47 -0500

    Complete attacks 0-1

--- lab11-security/attack.c
+++ lab11-security/attack.c
@@ -1,9 +1,18 @@
 #include "lab.h"
+#include <stdio.h>
+#include <string.h>
 
 /* forward function for "attack 0" case.
    change this code to implement your attack
  */
 void forward_attack_0(struct message *message) {
+    if (message->from == 'A') {
+        // Change message text
+        int amt = 10001438;
+        snprintf((char *) message->data, sizeof(message->data), "PAY $%d TO M", amt / 2);
+        message->data_size = strlen((char *) message->data);
+    }
+
     send_message(message);
 }
 
@@ -11,7 +20,19 @@ void forward_attack_0(struct message *message) {
    change this code to implement your attack
  */
 void forward_attack_1(struct message *message) {
-    send_message(message);
+    if (message->from == 'A') {
+        int amt = 10001438;
+        char new_text[1024];
+        snprintf(new_text, sizeof(new_text), "PAY $%d TO M", amt / 2);
+
+        // Send different message
+        struct message *new_msg = new_message(
+            'A', 'B', new_text, 1, 0
+        );
+        send_message(new_msg);
+    } else {
+        send_message(message);
+    }
 }
 
 /* forward function for "attack 2" case.
