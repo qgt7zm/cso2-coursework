3dde1166ade6ce796295b1a3d4cd6ce7687d1913
Eric Weng <qgt7zm@virginia.edu>
2023-11-13 20:57:32 -0500

    Add lab 11 starter files

---
+++ lab11-security/attack.c
@@ -0,0 +1,41 @@
+#include "lab.h"
+
+/* forward function for "attack 0" case.
+   change this code to implement your attack
+ */
+void forward_attack_0(struct message *message) {
+    send_message(message);
+}
+
+/* forward function for "attack 1" case.
+   change this code to implement your attack
+ */
+void forward_attack_1(struct message *message) {
+    send_message(message);
+}
+
+/* forward function for "attack 2" case.
+   change this code to implement your attack
+ */
+void forward_attack_2(struct message *message) {
+    send_message(message);
+}
+
+/* forward function for "attack 3" case.
+   change this code to implement your attack
+ */
+void forward_attack_3(struct message *message) {
+    send_message(message);
+}
+
+/* forward function for "attack 4" case.
+   change this code to implement your attack */
+void forward_attack_4(struct message *message) {
+    send_message(message);
+}
+
+/* forward function for "attack 5" case.
+   I did not intend this one to be possible. */
+void forward_attack_5(struct message *message) {
+    send_message(message);
+}
