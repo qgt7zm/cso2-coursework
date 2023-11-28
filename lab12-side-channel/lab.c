4b3047bb72b0958f71b24ee4924f1d2ce871447e
Eric Weng <qgt7zm@virginia.edu>
2023-11-28 14:39:38 -0500

    Retry if passphrase not found

--- lab12-side-channel/lab.c
+++ lab12-side-channel/lab.c
@@ -7,7 +7,8 @@
 #define NUM_LETTERS 26
 #define FIRST_LETTER 'a'
 #define NUM_TRIALS 10
-#define MAX_RETRY 5
+
+void guess_passphrase(char *buffer, int length);
 
 /* When complete, this function should fill in "buffer"
  * with a length-character \0-termianted string such that
@@ -25,13 +26,34 @@
  *  the passphrase is stored in another way.)
  */
 void find_passphrase(char *buffer, int length) {
+    const int max_guesses = length;
+
     // Warm up the processor
     memset(buffer, FIRST_LETTER, length);
     int result;
-    for (int i = 0; i < 10; i++) {
+
+    for (int i = 0; i < NUM_TRIALS; i++) {
         measure_once(&result, buffer, check_passphrase);
     }
+    
+    for (int i = 1; i <= max_guesses; i++) {
+        guess_passphrase(buffer, length);
+        // Check passphrase
+        measure_once(&result, buffer, check_passphrase);
+        if (result == 1) {
+            printf("Found correct passphrase after %d tries\n\n", i);
+            break;
+        } else if (i < max_guesses) {
+            printf("Found incorrect passphrase; retrying\n\n");
+        } else {
+            printf("Failed to find correct passphrase in %d tries\n\n", max_guesses);
+        }
+    }
+}
 
+// Helper function
+void guess_passphrase(char *buffer, int length) {
+    int result;
     memset(buffer, '\0', length);
 
     // Time each letter and find the max
@@ -55,15 +77,5 @@ void find_passphrase(char *buffer, int length) {
         }
         buffer[i] = FIRST_LETTER + maxIdx;
         printf("'%s' took %ld cycles\n", buffer, maxTiming / NUM_TRIALS);
-    }
-
-    // Check passphrase
-    measure_once(&result, buffer, check_passphrase);
-    if (result == 1) {
-        printf("Found correct passphrase\n");
-        return;
-    } else {
-        printf("Found incorrect passphrase\n");
-        // TODO retry
     }
 }
