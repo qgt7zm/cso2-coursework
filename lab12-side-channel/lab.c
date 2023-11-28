99c5373da15d0b9fae5f5d37f217257e24202733
Eric Weng <qgt7zm@virginia.edu>
2023-11-28 14:04:39 -0500

    Warmup processor

--- lab12-side-channel/lab.c
+++ lab12-side-channel/lab.c
@@ -3,6 +3,10 @@
 #include <string.h>     // for memset
 #include <stdio.h>      // for printf
 
+// Constants
+#define NUM_LETTERS 26
+#define FIRST_LETTER 'a'
+
 /* When complete, this function should fill in "buffer"
  * with a length-character \0-termianted string such that
  * check_passphrase(buffer) is true.
@@ -19,27 +23,24 @@
  *  the passphrase is stored in another way.)
  */
 void find_passphrase(char *buffer, int length) {
-    memset(buffer, 0, length);
+    // Warm up the processor
+    memset(buffer, 'c', length);
     int result;
-    buffer[0] = 'a';
-    long time_for_a = measure_once(&result, buffer, check_passphrase);
-    if (result == 1) {
-        // 'a' is correct passphrase, done
-        return;
+    for (int i = 0; i < 10; i++) {
+        measure_once(&result, buffer, check_passphrase);
     }
-    buffer[0] = 'b';
-    long time_for_b = measure_once(&result, buffer, check_passphrase);
-    if (result == 1) {
-        // 'b' is correct passphrase, done
-        return;
+
+    memset(buffer, '\0', length);
+    result = 0;
+
+    for (int i = 0; i < NUM_LETTERS; i++) {
+        char next_char = FIRST_LETTER + i;
+        buffer[0] = next_char;
+        long time_for_a = measure_once(&result, buffer, check_passphrase);
+        printf("'%c' took %ld cycles\n", next_char, time_for_a);
+        if (result == 1) {
+            // Found correct passphrase, done
+            return;
+        }
     }
-    buffer[0] = 'c';
-    long time_for_c = measure_once(&result, buffer, check_passphrase);
-    if (result == 1) {
-        // 'c' is correct passphrase, done
-        return;
-    }
-    printf("'a' took %ld cycles\n", time_for_a);
-    printf("'b' took %ld cycles\n", time_for_b);
-    printf("'c' took %ld cycles\n", time_for_c);
 }
